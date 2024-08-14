752050f1b91dbdcf62e4e9482f9905a944f54afb
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-08-13 22:21:22 -0300

    make larger datasets

--- 2024.1/lab2/make_dataset.sh
+++ 2024.1/lab2/make_dataset.sh
@@ -1,13 +1,20 @@
-#/bin/bash
+#!/bin/bash
 
-# generate $nfiles random content files in a dataset directory
+# generate $nfiles large files in a dataset directory
 nfiles=$1
+file_size=$2
+
+if [ -z "$nfiles" ] || [ -z "$file_size" ]; then
+    echo "Usage: $0 <number_of_files> <file_size_in_MB>"
+    exit 1
+fi
+
+# Convert file size to bytes
+file_size_bytes=$((file_size * 1024 * 1024))
 
 mkdir -p dataset
 
-for i in `seq 1 $nfiles`
-do
-	#from 0 to 32K
-	file_size=$RANDOM
-	head -c $file_size < /dev/urandom > dataset/file.$i
+for i in $(seq 1 $nfiles); do
+    # Generate a large file with the specified size
+    dd if=/dev/urandom of=dataset/file.$i bs=1M count=$file_size
 done
