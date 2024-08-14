0bf4c6f3f7f8bdc5a47739def0932872fbc5761c
Anthony Ferreira <anthony.ferreira@ccc.ufcg.edu.br>
2024-08-13 22:33:50 -0300

    Update make_dataset.sh

--- 2024.1/lab2/make_dataset.sh
+++ 2024.1/lab2/make_dataset.sh
@@ -2,19 +2,13 @@
 
 # generate $nfiles large files in a dataset directory
 nfiles=$1
-file_size=$2
 
-if [ -z "$nfiles" ] || [ -z "$file_size" ]; then
-    echo "Usage: $0 <number_of_files> <file_size_in_MB>"
-    exit 1
-fi
-
-# Convert file size to bytes
-file_size_bytes=$((file_size * 1024 * 1024))
+# File size in megabytes
+file_size_mb=10
 
 mkdir -p dataset
 
 for i in $(seq 1 $nfiles); do
     # Generate a large file with the specified size
-    dd if=/dev/urandom of=dataset/file.$i bs=1M count=$file_size
+    dd if=/dev/urandom of=dataset/file.$i bs=1M count=$file_size_mb status=progress
 done
